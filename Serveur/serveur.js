f5cf233f6fa16170366ba61ebde8b8db4dd6d7dc
Raahoul <raahoul.pandourangame@gmail.com>
2026-03-22 17:54:59 +0100

    Back-end et mis en place de Expresse serveur

--- Serveur/serveur.js
+++ Serveur/serveur.js
@@ -1,13 +1,59 @@
 import { createServer } from 'http';
 import { Server } from 'socket.io';
+import express from 'express';
 
-const httpServer = createServer();
+const app = express();
+const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
     origin: '*',
   },
 });
 
+app.use(express.json());
+// Tableau qui stocke les utilisateurs
+const users = [];
+
+// Créer un utilisateur
+app.post('/users', (req, res) => {
+  const { pseudo } = req.body;
+
+  if (!pseudo || pseudo.length < 3) {
+    return res.status(400).json({ error: "Pseudo invalide (minimum 3 caractères)" });
+  }
+
+  const exists = users.find(u => u.pseudo === pseudo);
+  if (exists) {
+    return res.status(400).json({ error: "Pseudo déjà utilisé" });
+  }
+
+  const user = { id: users.length + 1, pseudo };
+  users.push(user);
+
+  res.status(201).json(user);
+});
+
+// Récupérer tous les utilisateurs
+app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+// Login
+app.post('/login', (req, res) => {
+  const { pseudo } = req.body;
+
+  const user = users.find(u => u.pseudo === pseudo);
+
+  if (!user) {
+    return res.status(404).json({ error: "Utilisateur non trouvé" });
+  }
+
+  res.json({ message: "Connecté", user });
+});
+
+
+
+// la suite 
 io.on('connection', (socket) => {
   console.log('Un utilisateur vient de se connecter');
 
